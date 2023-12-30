c4cbe8797aa1541f51709c168569d147eb38a324
MGallo-Code <121553614+MGallo-Code@users.noreply.github.com>
2023-12-30 14:40:54 -0500

    Added step time delay and print statement to visualize time in GUI

--- simulate.py
+++ simulate.py
@@ -1,4 +1,13 @@
 import pybullet as p
+import time
 
+# Launch physics client GUI
 physicsClient = p.connect(p.GUI)
+
+for x in range(1000):
+    p.stepSimulation()
+    print(x)
+    time.sleep(1/60)
+
+# Close physics client GUI
 p.disconnect()
