1864d46ca91b5ccf4944f9f1f49e2a5cad7e11c8
MGallo-Code <121553614+MGallo-Code@users.noreply.github.com>
2023-12-30 18:56:28 -0500

    Created 'arch' joint layout

--- generate.py
+++ generate.py
@@ -25,30 +25,109 @@ def Create_Robot():
     # Define dimensions (L, W, H)
     torso_dim = (1, 1, 1)
     # Define positions (X, Y, Z) ABSOLUTE
-    torso_pos = (0, 0, torso_dim[2]/2)
+    torso_pos = (0, 0, 0.5)
     # Write to output
     pyrosim.Send_Cube(
-        name="Torso",
+        name="Link0",
         pos=[torso_pos[0], torso_pos[1], torso_pos[2]],
         size=[torso_dim[0], torso_dim[1], torso_dim[2]])
     
     # Define dimensions (L, W, H)
     leg_dim = (1, 1, 1)
     # Define positions (X, Y, Z) RELATIVE
-    leg_pos = (0, 0, leg_dim[2]/2)
+    leg_pos = (0, 0, 0.5)
     # Write to output
     pyrosim.Send_Cube(
-        name="Leg",
+        name="Link1",
+        pos=[leg_pos[0], leg_pos[1], leg_pos[2]],
+        size=[leg_dim[0], leg_dim[1], leg_dim[2]])
+    
+    # Define dimensions (L, W, H)
+    leg_dim = (1, 1, 1)
+    # Define positions (X, Y, Z) RELATIVE
+    leg_pos = (0, 0, 0.5)
+    # Write to output
+    pyrosim.Send_Cube(
+        name="Link2",
+        pos=[leg_pos[0], leg_pos[1], leg_pos[2]],
+        size=[leg_dim[0], leg_dim[1], leg_dim[2]])
+    
+    # Define dimensions (L, W, H)
+    leg_dim = (1, 1, 1)
+    # Define positions (X, Y, Z) RELATIVE
+    leg_pos = (0, 0.5, 0)
+    # Write to output
+    pyrosim.Send_Cube(
+        name="Link3",
+        pos=[leg_pos[0], leg_pos[1], leg_pos[2]],
+        size=[leg_dim[0], leg_dim[1], leg_dim[2]])
+    
+    # Define dimensions (L, W, H)
+    leg_dim = (1, 1, 1)
+    # Define positions (X, Y, Z) RELATIVE
+    leg_pos = (0, 0.5, 0)
+    # Write to output
+    pyrosim.Send_Cube(
+        name="Link4",
+        pos=[leg_pos[0], leg_pos[1], leg_pos[2]],
+        size=[leg_dim[0], leg_dim[1], leg_dim[2]])
+    
+    # Define dimensions (L, W, H)
+    leg_dim = (1, 1, 1)
+    # Define positions (X, Y, Z) RELATIVE
+    leg_pos = (0, 0, -0.5)
+    # Write to output
+    pyrosim.Send_Cube(
+        name="Link5",
+        pos=[leg_pos[0], leg_pos[1], leg_pos[2]],
+        size=[leg_dim[0], leg_dim[1], leg_dim[2]])
+    
+    # Define dimensions (L, W, H)
+    leg_dim = (1, 1, 1)
+    # Define positions (X, Y, Z) RELATIVE
+    leg_pos = (0, 0, -0.5)
+    # Write to output
+    pyrosim.Send_Cube(
+        name="Link6",
         pos=[leg_pos[0], leg_pos[1], leg_pos[2]],
         size=[leg_dim[0], leg_dim[1], leg_dim[2]])
 
     # Define our Torso_Leg joint
     pyrosim.Send_Joint(
-        name = "Torso_Leg",
-        parent= "Torso", child = "Leg",
+        name = "Link0_Link1",
+        parent= "Link0", child = "Link1",
         type = "revolute",
-        position = [0, 0, torso_dim[2]]) 
+        position = [0, 0, 1]) 
+    
+    pyrosim.Send_Joint(
+        name = "Link1_Link2",
+        parent= "Link1", child = "Link2",
+        type = "revolute",
+        position = [0, 0, 1]) 
 
+    pyrosim.Send_Joint(
+        name = "Link2_Link3",
+        parent= "Link2", child = "Link3",
+        type = "revolute",
+        position = [0, 0.5, 0.5]) 
+    
+    pyrosim.Send_Joint(
+        name = "Link3_Link4",
+        parent= "Link3", child = "Link4",
+        type = "revolute",
+        position = [0, 1, 0]) 
+    
+    pyrosim.Send_Joint(
+        name = "Link4_Link5",
+        parent= "Link4", child = "Link5",
+        type = "revolute",
+        position = [0, 0.5, -0.5])
+
+    pyrosim.Send_Joint(
+        name = "Link5_Link6",
+        parent= "Link5", child = "Link6",
+        type = "revolute",
+        position = [0, 0, -1])
     # Close file
     pyrosim.End()
 
