62029386c47973a1a368caba8509a53737d53c6f
MGallo-Code <121553614+MGallo-Code@users.noreply.github.com>
2023-12-30 16:23:13 -0500

    Created tower of boxes, decreased by 90% scale each time

--- generate.py
+++ generate.py
@@ -11,14 +11,20 @@ height = 1
 x = 0
 y = 0
 z = height/2
+# Create tower of cubes
+for _ in range(10):
+    # Write to output
+    pyrosim.Send_Cube(name="Box", pos=[x, y, z] , size=[length, width, height])
+    # Set our 'ground level' for next cube to the top of the next cube
+    z += height/2
 
-# Write to output
-pyrosim.Send_Cube(name="Box", pos=[x, y, z] , size=[length, width, height])
+    # Multiply scale by 0.9
+    length *= .9
+    width *= .9
+    height *= .9
 
-# Define second cube position variables
-x = 1
-z = height/2 + 1
-pyrosim.Send_Cube(name="Box", pos=[x, y, z] , size=[length, width, height])
+    # Add half of our new height to place the cube directly in the correct position
+    z += height/2
 
 # Close sdf file
 pyrosim.End()
