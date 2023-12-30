03e7222293973040eed9e7671e1b7f5c6fc5bcc2
MGallo-Code <121553614+MGallo-Code@users.noreply.github.com>
2023-12-30 15:25:34 -0500

    Configured simulate.py to load and run box

--- simulate.py
+++ simulate.py
@@ -3,6 +3,11 @@ import time
 
 # Launch physics client GUI
 physicsClient = p.connect(p.GUI)
+# Hide side bars in GUI
+p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+
+# Load predefined object from file
+p.loadSDF("box.sdf")
 
 for x in range(1000):
     p.stepSimulation()
