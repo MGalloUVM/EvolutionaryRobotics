f697f3977cde619089946802ff79da09bbe39f9b
MGallo-Code <121553614+MGallo-Code@users.noreply.github.com>
2023-12-31 16:02:48 -0500

    Cleaned up extra code in constants.py

--- constants.py
+++ constants.py
@@ -11,18 +11,3 @@ simulation_length = 1000
 sleep_per_frame = 1/120
 # Max force on a joint
 max_force = 25
-
-
-###
-# Motor Vector Generation
-###
-
-# BACK LEG
-bl_amplitude = pi/3
-bl_frequency = 0.05
-bl_phase_offset = 0
-
-# FRONT LEG
-fl_amplitude = pi/4
-fl_frequency = 0.05
-fl_phase_offset = pi/4
