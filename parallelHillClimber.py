d685212ce5c8db910fce98922f5069007c6242ac
MGallo-Code <121553614+MGallo-Code@users.noreply.github.com>
2023-12-31 18:35:48 -0500

    Created parallelHillClimber.py (10)

---
+++ parallelHillClimber.py
@@ -0,0 +1,36 @@
+import constants as c
+import copy
+from solution import SOLUTION
+
+
+class PARALLEL_HILL_CLIMBER:
+    def __init__(self):
+        self.parent = SOLUTION()
+    
+    def Evolve(self):
+        self.parent.Evaluate("GUI")
+        for currentGeneration in range(c.numberOfGenerations):
+            self.Evolve_For_One_Generation()
+    
+    def Evolve_For_One_Generation(self):
+        self.Spawn()
+        self.Mutate()
+        self.child.Evaluate("DIRECT")
+        self.Print()
+        self.Select()
+    
+    def Spawn(self):
+        self.child = copy.deepcopy(self.parent)
+
+    def Mutate(self):
+        self.child.Mutate()
+
+    def Select(self):
+        if self.child.fitness < self.parent.fitness:
+            self.parent = self.child
+            
+    def Show_Best(self):
+        self.parent.Evaluate("GUI")
+    
+    def Print(self):
+        print(f"Parent Fitness: {self.parent.fitness}, Child Fitness: {self.child.fitness}")
