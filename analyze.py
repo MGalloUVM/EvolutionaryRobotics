a14d1f5eee292c747406112e4a87ef19fec385df
MGallo-Code <121553614+MGallo-Code@users.noreply.github.com>
2023-12-30 20:59:42 -0500

    Fixed display style for better visibility

--- analyze.py
+++ analyze.py
@@ -4,6 +4,7 @@ import numpy as np
 backLegSensorValues = np.load('data/BackLegTouch.npy')
 frontLegSensorValues = np.load('data/FrontLegTouch.npy')
 
-plt.plot(frontLegSensorValues)
-plt.plot(backLegSensorValues)
+plt.plot(frontLegSensorValues, label="Front Leg Sensor")
+plt.plot(backLegSensorValues, label="Back Leg Sensor", color="red", alpha=0.6, linewidth=1)
+plt.legend()
 plt.show()
