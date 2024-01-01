84b5443b8ecfa9ee68c565abba8514d5904192c5
MGallo-Code <121553614+MGallo-Code@users.noreply.github.com>
2023-12-31 20:45:49 -0500

    Added cleanup functions for leftover files

--- search.py
+++ search.py
@@ -2,10 +2,12 @@ import os
 
 from parallelHillClimber import PARALLEL_HILL_CLIMBER
 
+# Remove any lingering files
+os.system("rm body.urdf; rm world.sdf; rm brain*.nndf; rm fitness*.txt")
 
 phc = PARALLEL_HILL_CLIMBER()
 phc.Evolve()
 phc.Show_Best()
 
-# Remove Body and World Files
+# Clean up Body and World files
 os.system("rm body.urdf; rm world.sdf")
